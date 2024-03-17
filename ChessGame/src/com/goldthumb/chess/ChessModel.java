4ca091752a2ed1c5f8ee617c97d088a707a190f5
Madhunicka Mathanamohan <madhunicka.25@gmail.com>
2024-03-17 14:53:07 +0530

    Testing

--- ChessGame/src/com/goldthumb/chess/ChessModel.java
+++ ChessGame/src/com/goldthumb/chess/ChessModel.java
@@ -1,5 +1,21 @@
 package com.goldthumb.chess;
 
 public class ChessModel {
+	
+	public String toString() {
+		String desc = "";
+		for(int row=7; row>=0; row--) {
+			desc+=""+row;
+			for(int col=0; col<8; col++) {
+				desc+=" .";
+				
+			}
+			desc+="\n";
+		}
+		
+		desc+="  0 1 2 3 4 5 6 7";
+		
+		return desc;
+	}
 
 }
